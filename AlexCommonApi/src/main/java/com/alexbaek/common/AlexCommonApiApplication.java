c823f5c204b081a9fb804046dcf254b1b87747aa
alexbaek <alexbaek31@gmail.com>
2017-02-24 18:33:50 +0900

    프로젝트 생성.

---
+++ AlexCommonApi/src/main/java/com/alexbaek/common/AlexCommonApiApplication.java
@@ -0,0 +1,12 @@
+package com.alexbaek.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexCommonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexCommonApiApplication.class, args);
+	}
+}
