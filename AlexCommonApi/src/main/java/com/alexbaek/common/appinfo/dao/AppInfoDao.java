36ebdd5b598f574a1de52e2b924a6748abd258f8
alexbaek <alexbaek31@gmail.com>
2017-03-20 18:44:07 +0900

    - 프로젝트 다시 생성.

---
+++ AlexCommonApi/src/main/java/com/alexbaek/common/appinfo/dao/AppInfoDao.java
@@ -0,0 +1,32 @@
+/***************************************************************************************************
+*   이 소스는 미디어로그 소유입니다.
+*   이 소스를 무단으로 도용하면 법에 따라 처벌을 받을 수 있습니다.
+*   @클래스명  : AppInfoDao.java
+*   @패키지명  : com.alexbaek.common.appinfo.dao
+*   @작성자	: 백우영
+*   @작성일	: 2017. 3. 20. 오전 11:48:43
+************************** @수정이력 ****************************************************************
+*  수정일    수정자   수정내용
+*     
+***************************************************************************************************/
+package com.alexbaek.common.appinfo.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.alexbaek.common.appinfo.vo.AppInfoVO;
+
+/**
+ * The Class AppInfoDao.java.
+ * 설명 :
+ *
+ * @author 백우영
+ * @version 1.0
+ */
+@Component
+public class AppInfoDao {
+	
+	public AppInfoVO selectAppInfo(AppInfoVO appInfo) {
+		return null;
+		// return this.sqlSession.selectOne("selectAppInfo", appInfo);
+	}
+}
